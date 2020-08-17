592056c641b9530f75322222f31a4765fc3769bc
tungpham286 <levelsilver213@gmail.com>
2020-08-17 21:43:08 +0700

    lmao

---
+++ Session-3/H-Video/02_CreateEx/05_Array_CreateEx/ex.js
@@ -0,0 +1,4 @@
+let movie =['MadMax: Fury Road','Running in the 90s'];
+
+let item = prompt("Con phim gi nua khong?");
+movie.push(item);
---
+++ Session-3/Video/03_Array_Create/create.js
@@ -0,0 +1,9 @@
+let menu = ['cho','meo','ga','cho nhung bi ngu'];
+console.log(menu);
+
+menu.push("bo`");
+console.log(menu);
+
+let item = prompt("Dinh an gi nua khong?");
+menu.push(item);
+console.log(menu);
